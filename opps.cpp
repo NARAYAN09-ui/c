f143e86855ffdd80120fbd77be7e99910d02b783
NARAYAN09-ui <narayan.cs241202@global.org.in>
2025-09-25 18:11:33 +0530

    Update oppm.cpp

--- opps.cpp
+++ opps.cpp
@@ -1,9 +1,19 @@
-#include<iostream>
-
+#include <iostream>
+using namespace std;
 class myclass
-{
-     int x,y;
-    public:
-    
-
+{int a;
+  float b;
+  public:
+  myclass(){a=1;
+    b=2;
 } 
+ };
+int add(int x, int y) {
+    return x + y;
+}
+
+int main() {
+    int a=5, b=10;
+    cout<<"Sum:"<< add(a,b)<<endl;
+    return 0;
+}
