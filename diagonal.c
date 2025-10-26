6a1e4792149cd50050acc849e0dfa6022ec8b568
Narayan Patel <narayan.cs241202@global.org.in>
2025-10-26 20:28:41 +0530

    Create diagonal.c

---
+++ diagonal.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter the size of the square matrix: ");
+    scanf("%d", &n);
+
+    int matrix[n][n];
+
+    printf("Enter the elements of the matrix:\n");
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            scanf("%d", &matrix[i][j]);
+
+    printf("\nPrimary diagonal: ");
+    for (int i = 0; i < n; i++)
+        printf("%d ", matrix[i][i]);
+
+    printf("\nSecondary diagonal: ");
+    for (int i = 0; i < n; i++)
+        printf("%d ", matrix[i][n - i - 1]);
+
+    return 0;
+}
